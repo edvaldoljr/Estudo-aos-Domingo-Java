9e62dac3ce8f55d098ed71817cc4864bf2d856bf
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-02 13:53:39 -0300

    Variaveis java

---
+++ src/Variaveis.java
@@ -0,0 +1,30 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        boolean casadoVerdade = true;
+        boolean solteiroFalso = false;
+        char caracterUnico = 'A';
+        char caracterUnicod = '\u0041';
+        String nomeAluno = "Edvaldo";
+        byte idadeByte = 127;
+        short idadeCidade = 32767;
+        int idadeCidade2 = 327678214;
+        long numeroLong = 3276782144L;
+        float numeroPontoFlutuante = 12.4F;
+        double numeroDouble = 4551524619684.574;
+
+        // Isso é um comentario
+        System.out.println("true:" + casadoVerdade);
+        System.out.println("Solteiro:" + solteiroFalso);
+        System.out.println("Caracter Unico: " + caracterUnico);
+        System.out.println("Unicode: " + caracterUnicod);
+        System.out.println("Nome: " + nomeAluno);
+        System.out.println("Idade: " + idadeByte);
+        System.out.println("Idade cidade: " + idadeCidade);
+        System.out.println("Idade cidade 2: " + idadeCidade2);
+        System.out.println("Numero Long: " +numeroLong);
+        System.out.println("Numero Ponto Flutuante: " + numeroPontoFlutuante);
+        System.out.println("Numero Double: " + numeroDouble);
+
+
+    }
+}
