a8ed19ffd510375f3d137c2f03ab62a306f05d48
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-02 17:52:31 -0300

    Operados Logicos

---
+++ src/OperadoresLogioco.java
@@ -0,0 +1,14 @@
+public class OperadoresLogioco {
+    public static void main(String[] args) {
+
+        // && ||
+
+        int idade = 15;
+        double altura = 1.60;
+
+        System.out.println(idade >= 18 || altura >= 1.60 );
+        System.out.println(idade >= 18 && altura >= 1.60 );
+        System.out.println(idade >= 18 && altura >= 1.60 );
+
+    }
+}
