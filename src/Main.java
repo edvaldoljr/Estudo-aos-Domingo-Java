bcec87f08b2661c7b1edc037c73932ab8b04e046
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-02 13:10:31 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        boolean casadoVerdadeiro = true;
-        boolean solteiroFalso = false;
-        char caractere = 'A';
-        char caractereHexadecimal = '\u0041';
-        String nomeAluno = "Edvaldo";
-        byte idadeByte = -128;
-        short idadeShort = 32767;
-        int idadeDoAluno = 2147483;
-        long numeroGrande = 263;
-        float pontoFlutuante = 3.40282346638528860F;
-        double pontoFlutuanteDobroDeCapacidade = 1.79769313486231570D;
+        System.out.println("Olá Mundo");
     }
 }
