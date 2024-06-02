e9eb59298ebc03c76ce3e205efffb0a8d54f0574
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-02 17:43:18 -0300

    Operadores de Comparação

---
+++ src/OperadoresComparacao.java
@@ -0,0 +1,13 @@
+public class OperadoresComparacao {
+    public static void main(String[] args) {
+        int numeroUm = 10;
+        int numeroDois = 30;
+
+        System.out.println(numeroUm > numeroDois);
+        System.out.println(numeroUm >= numeroDois);
+        System.out.println(numeroUm < numeroDois);
+        System.out.println(numeroUm <= numeroDois);
+        System.out.println(numeroUm == numeroDois);
+        System.out.println(numeroUm != numeroDois);
+    }
+}
